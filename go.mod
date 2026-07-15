700bfd137c328f9e0642d2c01fd09619d9190dd3
27 <27@riseup.net>
2026-07-15 16:17:47 -0500

    -type LicenseError struct {

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module 41.neocities.org/diana
 go 1.26.0
 
 require (
-	41.neocities.org/protobuf v1.7.2
+	41.neocities.org/protobuf v1.7.3
 	filippo.io/nistec v0.0.4
 	github.com/emmansun/gmsm v0.44.0
 )
--- widevine/errors.go
+++ widevine/errors.go
@@ -3,27 +3,14 @@ package widevine
 
 import (
    "41.neocities.org/protobuf"
+   "errors"
    "fmt"
 )
 
-// decodeErrorFromMessage constructs a LicenseError struct from a pre-parsed
-// protobuf message
 func decodeErrorFromMessage(message protobuf.Message) error {
-   errorCode, _ := message.Field(1)
-   return &LicenseError{
-      ErrorCode: errorCode,
+   errorCode, ok := message.Field(1)
+   if !ok || errorCode == nil {
+      return errors.New("widevine license error: unknown code")
    }
-}
-
-// LicenseError reflects the structure of the Widevine LicenseError protobuf.
-type LicenseError struct {
-   ErrorCode *protobuf.Field
-}
-
-// Error implements the standard Go error interface.
-func (le *LicenseError) Error() string {
-   if le.ErrorCode == nil {
-      return "widevine license error: unknown code"
-   }
-   return fmt.Sprint("widevine license error: code ", le.ErrorCode.Numeric)
+   return fmt.Errorf("widevine license error: code %v", errorCode.Numeric)
 }
